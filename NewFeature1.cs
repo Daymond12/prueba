e987095986d4f2239ce7dc6199616068847b98e3
Roger Davila <droger88@hotmail.com>
2019-08-30 03:08:17 -0600

    Suma1

--- NewFeature1.cs
+++ NewFeature1.cs
@@ -4,5 +4,13 @@ public class Class1
 {
 	public Class1()
 	{
+        int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1+x2;
+            return sum;
+
+        }
 	}
 }
